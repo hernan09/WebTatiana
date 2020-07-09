b3867f3c36d9360e025fc98b3e108189abd8725f
hernan <hernan09.gz@gmail.com>
2020-07-09 16:21:39 -0300

    asda

---
+++ src/app/pregfrecuentes/pregfrecuentes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pregfrecuentes',
+  templateUrl: './pregfrecuentes.component.html',
+  styleUrls: ['./pregfrecuentes.component.scss']
+})
+export class PregfrecuentesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
