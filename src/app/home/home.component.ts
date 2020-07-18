d6943cc86a36a0e5a99170ddd08da9bee36fe6de
insfran23emiliano <emilianoinsfran@gmail.com>
2020-07-18 20:04:56 -0300

    server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { HttpClient } from '@angular/common/http';
+import { UtilsService } from '../../app/utils.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-arrayImages=[]
-  constructor(private router: Router) { }
+  arrayImages=[]
+  constructor(private router: Router, private utilsService:UtilsService) { }
 
   ngOnInit(): void {
     this.getImages()
@@ -55,4 +58,172 @@ arrayImages=[]
       
     ]
   }
+
+
+
+  getCategoria(){
+    console.log("estoy en categorias GET");//_id que te genera mongo
+
+    this.utilsService.getConfig('http://localhost:3000/categoria')
+      .subscribe((data) => {
+        console.log("data->",data);
+      });
+
+  }
+
+  postCategoria(){
+    console.log("estoy en categorias POST");
+
+    let obj = {
+        id_admin:1,
+        nombre: 'Madera3',
+        descripcion: 'Maderapara exteriores',
+        disponible: true,
+        stock: 10,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/16_wood_samples.jpg'
+    }
+
+    this.utilsService.postConfig('http://localhost:3000/categoria',obj)
+      .subscribe(
+        (data) => {
+          console.log("data->",data);
+        },
+        err =>{
+          console.log("ERROR",err);
+        }
+
+      );
+
+  }
+
+
+  putCategoria(){
+    console.log("estoy en categorias PUT");
+
+    let obj = {
+        id_admin:1,
+        nombre: 'Madera testtest final',
+        descripcion: 'Maderapara exteriores',
+        disponible: true,
+        stock: 10000000000000,
+    }
+
+    let id = "5f112fc514efaa5550b03771"
+
+    this.utilsService.putConfig('http://localhost:3000/categoria/'+id,obj)
+      .subscribe(
+        (data) => {
+          console.log("data->",data);
+        },
+        err =>{
+          console.log("ERROR",err);
+        }
+
+      );
+
+  }
+
+
+  deleteCategoria(){
+    console.log("estoy en categorias DELETE");
+
+    let id = "5f112fc514efaa5550b03771";
+
+    this.utilsService.deleteConfig('http://localhost:3000/categoria/'+id)
+      .subscribe(
+        (data) => {
+          console.log("data->",data);
+        },
+        err =>{
+          console.log("ERROR",err);
+        }
+
+      );
+
+  }
+
+
+
+
+  //---------------------------------Porducto
+
+  getProducto(){
+    console.log("estoy en producto GET");
+    let id= 1;
+    this.utilsService.getConfig('http://localhost:3000/producto/'+id)
+      .subscribe((data) => {
+        console.log("data->",data);
+      });
+  }
+
+  postProducto(){
+    console.log("estoy en producto POST");
+
+    let obj = {
+        id_admin:1,
+        id_catalogo:2,
+        nombre: 'textil 1',
+        descripcion: 'Maderapara exteriores',
+        disponible: true,
+        stock: 10,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/16_wood_samples.jpg'
+    }
+
+    this.utilsService.postConfig('http://localhost:3000/producto',obj)
+      .subscribe(
+        (data) => {
+          console.log("data->",data);
+        },
+        err =>{
+          console.log("ERROR",err);
+        }
+
+      );
+
+  }
+
+
+  putProducto(){
+    console.log("estoy en producto PUT");
+
+    let obj = {
+        id_admin:1,
+        id_catalogo:1,
+        nombre: 'Madera testtest update',
+        descripcion: 'Maderapara exteriores',
+        disponible: true,
+        stock: 10000000000000,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/16_wood_samples.jpg'
+    }
+
+    let id = "5f137680af4ee3182872d0bd";
+
+    this.utilsService.putConfig('http://localhost:3000/producto/'+id,obj)
+      .subscribe(
+        (data) => {
+          console.log("data->",data);
+        },
+        err =>{
+          console.log("ERROR",err);
+        }
+
+      );
+  }
+
+  deleteProducto(){
+    console.log("estoy en producto DELETE");
+
+    let id = "5f137680af4ee3182872d0bd";
+
+    this.utilsService.deleteConfig('http://localhost:3000/producto/'+id)
+      .subscribe(
+        (data) => {
+          console.log("data->",data);
+        },
+        err =>{
+          console.log("ERROR",err);
+        }
+
+      );
+  } 
 }
--- src/app/utils.service.ts
+++ src/app/utils.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilsService {
   data:any;
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   setData(data){
     this.data = data;
@@ -14,4 +17,30 @@ export class UtilsService {
   getData(){
     return this.data;
   }
+
+
+
+	getConfig(url) {
+		console.log("url",url);
+		return this.http.get(url);
+	}
+
+
+	postConfig(url,obj) {
+		console.log("url",url);
+		console.log("obj",obj);
+		return this.http.post(url,obj);
+	}
+
+	putConfig(url,obj) {
+		console.log("url",url);
+		console.log("obj",obj);
+		return this.http.put(url,obj);
+	}
+
+	deleteConfig(url) {
+		console.log("url",url);
+		return this.http.delete(url);
+	}
+
 }
